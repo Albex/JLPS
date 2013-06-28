83db18b8f6fe6712c84c0f848c1d81871362d548
Alexandre Camus <alex.camus@ymail.com>
2013-06-28 14:44:43 +0100

    creation of Action and ClauseAtom

---
+++ src/Action.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Albex
+ * Date: 28/06/13
+ * Time: 14:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class Action extends Predicate {
+
+    public Action (String name, String[] variables) {
+        super(name, variables);
+    }
+
+}
---
+++ src/ClauseAtom.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Albex
+ * Date: 28/06/13
+ * Time: 14:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClauseAtom extends Predicate {
+
+    public ClauseAtom (String name, String[] variables) {
+        super(name, variables);
+    }
+
+}
