4d554cd55b31de27bf5273ccadaf04e0ac8bd81e
Alexandre Camus <alex.camus@ymail.com>
2013-07-18 11:01:14 +0100

    Debug and rename

--- src/controller/Interpreter.java
+++ src/controller/Interpreter.java
@@ -1,12 +1,10 @@
 package controller;
 import java.rmi.RemoteException;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import model.Constant;
 import model.DPostDeclaration;
 import model.Initiator;
-import model.Predicate;
 import model.SimpleSentence;
 import model.Terminator;
 import model.Unifiable;
@@ -50,6 +48,44 @@ public class Interpreter {
 		 return Interpreter.instance;
     }
     
+    public boolean isConstant(String string) {
+    	string = string.replaceAll(" ", "");
+    	
+    	return string.matches("[a-z0-9][A-Za-z0-9_]*");
+    }
+    
+    public Constant stringToConstant(String string) throws RemoteException {
+    	string = string.replaceAll(" ", "");
+    	Constant res;
+    	
+        if (this.isConstant(string)) {
+            res = new Constant(string);
+
+            return res;
+        } else {
+            throw new RemoteException("It is not a Constant-like string.");
+        }
+    }
+    
+    public boolean isVariable(String string) {
+    	string = string.replaceAll(" ", "");
+    	
+    	return string.matches("[A-Z][A-Za-z0-9_]*");
+    }
+    
+    public Variable stringToVariable(String string) throws RemoteException {
+    	string = string.replaceAll(" ", "");
+    	Variable res;
+    	
+        if (this.isVariable(string)) {
+            res = new Variable(string);
+
+            return res;
+        } else {
+            throw new RemoteException("It is not a Variable-like string.");
+        }
+    }
+    
     /**
      * Checks if the input string is in the form of a Prolog predicate.
      * This string should have the form of 'someLettersOrNumbersOr_(aValue,anotherValue,AVariable,AnotherVariable)'.
@@ -58,13 +94,18 @@ public class Interpreter {
      * @param string to be checked whether it is a Prolog predicate or not.
      * @return true if it matches the general form of a Prolog predicate, otherwise false.
      */
-    public boolean isProlog(String string) {
-        boolean res = false;
-        if (string.matches("[^(,|\\(|\\))]*\\(([^(,|\\(|\\))]+(,[^(,|\\(|\\))]+)*)*\\)")) {
-            res = true;
+    public boolean isSimpleSentence(String string) {
+    	string = string.replaceAll(" ", "");
+    	String[] strings = string.split("\\(|,|\\)");
+        
+        boolean predicateName = isConstant(strings[0]);
+        
+        boolean parameters = true;
+        for(int i = 1; i < strings.length; i++) {
+        	parameters = (isConstant(strings[i]) || isVariable(strings[i])) ? parameters : false;
         }
 
-        return res;
+        return predicateName && parameters;
     }
 
     /**
@@ -73,17 +114,20 @@ public class Interpreter {
      * @return a <code>Predicate</code> object. Then it will need to be stored in some place of the LPS framework.
      * @throws RemoteException if the string is not a Prolog predicate according to the <code>isProlog</code> method.
      */
-    public Predicate prologToPredicate(String string) throws RemoteException {
-        Predicate res;
-        if (this.isProlog(string)) {
+    public SimpleSentence stringToSimpleSentence(String string) throws RemoteException {
+    	string = string.replaceAll(" ", "");
+    	SimpleSentence res;
+        if (this.isSimpleSentence(string)) {
             String[] nameAndVar = string.split("\\(|,|\\)");
+            Constant predicateName = new Constant(nameAndVar[0]);
+            Unifiable[] parameters = new Unifiable[nameAndVar.length - 1];
             if (nameAndVar.length > 1) {
 	            for (int i = 1; i < nameAndVar.length; i++) {
-	                nameAndVar[i] = (nameAndVar[i].matches("[A-Z].*")) ? null : nameAndVar[i];
+	                parameters[i-1] = (nameAndVar[i].matches("[A-Z].*")) ? new Variable(nameAndVar[i]) : new Constant(nameAndVar[i]);
 	            }
-	            res = new Predicate(nameAndVar[0], Arrays.copyOfRange(nameAndVar, 1, nameAndVar.length));
+	            res = new SimpleSentence(predicateName, parameters);
             } else {
-            	res = new Predicate(nameAndVar[0], null);
+            	res = new SimpleSentence(predicateName);
             }
 
             return res;
@@ -100,12 +144,26 @@ public class Interpreter {
      * @return true if the string is a DPost declaration, otherwise false.
      */
     public boolean isDPostDeclaration(String string) {
-    	boolean res = false;
-    	if (string.matches("[^(,|\\(|\\))]*\\([^(,|\\(|\\))]*\\(([^(,|\\(|\\))]+(,[^(,|\\(|\\))]+)*)*\\),([^(,|\\(|\\))]*\\(([^(,|\\(|\\))]+(,[^(,|\\(|\\))]+)*)*\\))\\)")) {
-            res = true;
-        }
-    	
-    	return res;
+    	string = string.replaceAll(" ", ""); 
+    	String[] strings = string.split("\\(");
+    	String[] substrings = strings[2].split(",|\\)");
+    	String[] substrings2 = strings[3].split(",|\\)");
+		 
+    	boolean declarationName = strings[0].equals("initiates") || strings[0].equals("terminates");
+		
+    	boolean eventName = isConstant(strings[1]);
+		boolean eventParameters = true;
+		for(int i = 0; i < substrings.length - 2; i++) {
+			eventParameters = (isConstant(substrings[i]) || isVariable(substrings[i])) ? eventParameters : false;
+		}
+		 
+		boolean predicateName = isConstant(substrings[substrings.length - 1]);
+		boolean predicateParameters = true;
+		for(int i = 0; i < substrings2.length - 1; i++) {
+			predicateParameters = (isConstant(substrings2[i]) || isVariable(substrings2[i])) ? predicateParameters : false;
+		}
+		
+		return declarationName && eventName && eventParameters && predicateName && predicateParameters;
     }
     
     /**
@@ -116,7 +174,8 @@ public class Interpreter {
      * It depends on the nature of the DPost declaration, whether it is an 'initiates' or 'a 'terminates'.
      * @throws RemoteException if the string is not a DPost declaration according to the method <code>isDPostDeclaration</code>.
      */
-    public DPostDeclaration prologToDPost(String string) throws RemoteException {
+    public DPostDeclaration stringToDPost(String string) throws RemoteException {
+    	string = string.replaceAll(" ", "");
     	DPostDeclaration res;
     	
     	if (this.isDPostDeclaration(string)) {
@@ -135,10 +194,10 @@ public class Interpreter {
     			for (int i = 1; i < event.length; i++) {
     				if (event[i].matches("[A-Z].*")) {
     					Variable v = new Variable(event[i]);
-    					parametersOfEvent[i] = v;
+    					parametersOfEvent[i-1] = v;
     					variablesOfEvent.put(event[i], v);
     				} else {
-    					parametersOfEvent[i] = new Constant(event[i]);
+    					parametersOfEvent[i-1] = new Constant(event[i]);
     				}
 	            }
     			e = new SimpleSentence(eventName, parametersOfEvent);
@@ -152,15 +211,17 @@ public class Interpreter {
     		SimpleSentence p;
     		Constant predicateName = new Constant(predicate[1]);
     		if (predicate.length > 2) {
-    			for (int i = 1; i < predicate.length; i++) {
+    			for (int i = 2; i < predicate.length; i++) {
     				// Seeking if the variable is in the event ones and then adding an entry in the linkedVariables.
     				if (predicate[i].matches("[A-Z].*")) {
     					if (variablesOfEvent.containsKey(predicate[i])) {
-    						parametersOfPredicate[i] = variablesOfEvent.get(predicate[i]);
+    						parametersOfPredicate[i-2] = variablesOfEvent.get(predicate[i]);
     					} else {
-    						parametersOfPredicate[i] = new Constant(predicate[i]);
+    						parametersOfPredicate[i-2] = new Variable(predicate[i]);
     					}
-    				}
+    				} else {
+						parametersOfPredicate[i-2] = new Constant(predicate[i]);
+					}
 	            }
     			p = new SimpleSentence(predicateName, parametersOfPredicate);
     		} else {
