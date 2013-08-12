be772c4cac355c8ea06dc707c442c8c04350ce7e
Alexandre Camus <alex.camus@ymail.com>
2013-08-12 23:22:49 +0100

    Change name and add the file input for the dining philosophers

---
+++ src/main/JLPS.java
@@ -0,0 +1,148 @@
+package main;
+
+import java.io.IOException;
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.TokenStream;
+import org.fusesource.jansi.AnsiConsole;
+
+import model.CycleHandler;
+import model.Database;
+import model.GoalsList;
+import model.ReactiveRuleSet;
+import controller.syntax.JLPSSyntaxLexer;
+import controller.syntax.JLPSSyntaxParser;
+
+
+public class JLPS {
+
+	public JLPS() {
+	}
+	
+	public static CharStream fileOpener(String path, boolean absolutePath) throws IOException {
+		if (!absolutePath) {
+			path = System.getProperty("user.dir") + path;
+		}
+		
+		return new ANTLRFileStream(path);
+	}
+	
+	public static void fileReader(CharStream fileStream) throws RecognitionException {
+		JLPSSyntaxLexer lexer = new JLPSSyntaxLexer(fileStream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		JLPSSyntaxParser parser = new JLPSSyntaxParser(tokenStream);
+		boolean[] warnings = parser.file();
+		
+		if (warnings[0]) {
+			AnsiConsole.out.println("\u001B[33m" + "/!\\ No initial facts defined" + "\u001B[37m");
+		}
+		if (warnings[1]) {
+			AnsiConsole.out.println("\u001B[33m" + "/!\\ No intensional rules defined" + "\u001B[37m");
+		}
+		if (warnings[2]) {
+			AnsiConsole.out.println("\u001B[33m" + "/!\\ No domain theory defined" + "\u001B[37m");
+		}
+		if (warnings[3]) {
+			AnsiConsole.out.println("\u001B[33m" + "/!\\ No reactive rules defined" + "\u001B[37m");
+		}
+		if (warnings[4]) {
+			AnsiConsole.out.println("\u001B[33m" + "/!\\ No goals defined" + "\u001B[37m");
+		}
+		if (warnings[5]) {
+			AnsiConsole.out.println("\u001B[33m" + "/!\\ No initial events defined" + "\u001B[37m");
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			fileReader(fileOpener(args[0], true));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (RecognitionException e1) {
+			e1.printStackTrace();
+		}
+		
+		Database.getInstance().printOut();
+		System.out.println("Reactive rules: {\n" + ReactiveRuleSet.getInstance().toString() + "\n}\n");
+		System.out.println("Events: \n" + CycleHandler.getInstance().getEvents());
+		
+		for(int i = 1; i < 8; i++) {
+			CycleHandler.getInstance().handlerMethod("\nUPDATE " + i);
+			Database.getInstance().printOut();
+			CycleHandler.getInstance().handlerMethod("\nFIRING " + i);
+			CycleHandler.getInstance().handlerMethod("\nSOLVING " + i);
+			System.out.println(GoalsList.getInstance().toString());
+			System.out.println("Events: \n" + CycleHandler.getInstance().getEvents());
+		}
+	}
+
+	//public static void philosopher() throws RemoteException {
+		// Database {
+		// Facts {
+		/*SimpleSentence f1 = Interpreter.stringToSimpleSentence("available(f0)", null);
+		SimpleSentence f2 = Interpreter.stringToSimpleSentence("available(f1)", null);
+		SimpleSentence f3 = Interpreter.stringToSimpleSentence("available(f2)", null);
+		SimpleSentence f4 = Interpreter.stringToSimpleSentence("available(f3)", null);
+		SimpleSentence f5 = Interpreter.stringToSimpleSentence("available(f4)", null);
+		FactSet S = new FactSet(f1, f2, f3, f4, f5);
+		// }
+		// Rules {
+		Rule r1 = Interpreter.stringToRule("adjacent(f0, philosopher(0), f1)", null);
+		Rule r2 = Interpreter.stringToRule("adjacent(f1, philosopher(1), f2)", null);
+		Rule r3 = Interpreter.stringToRule("adjacent(f2, philosopher(2), f3)", null);
+		Rule r4 = Interpreter.stringToRule("adjacent(f3, philosopher(3), f4)", null);
+		Rule r5 = Interpreter.stringToRule("adjacent(f4, philosopher(4), f0)", null);
+		RuleSet L = new RuleSet(r1, r2, r3, r4, r5);
+		// }*/
+		/*// DSet {
+		// {
+		HashMap<String, Variable> variables = new HashMap<String, Variable>();
+		SimpleSentence a1 = Interpreter.stringToSimpleSentence("pickupForks(F1, philosopher(I), F2)", variables);
+		ArrayList<Terminator> terma1 = new ArrayList<Terminator>();
+		terma1.add((Terminator) Interpreter.stringToDPost("terminates(pickupForks(F1, philosopher(I), F2), available(F1))", variables));
+		terma1.add((Terminator) Interpreter.stringToDPost("terminates(pickupForks(F1, philosopher(I), F2), available(F2))", variables));
+		Clause conda1 = Interpreter.stringToClause("available(F1) & available(F2)", variables);
+		Clause confa1 = Interpreter.stringToAnd("!pickupForks(F2, philosopher(J), F3) & !pickupForks(F3, philosopher(K), F1)", variables);
+		Action pickup = new Action(a1, new ArrayList<Initiator>(), terma1, conda1, confa1);
+		// }
+		// {
+		variables = new HashMap<String, Variable>();
+		SimpleSentence a2 = Interpreter.stringToSimpleSentence("putdownForks(F1, philosopher(I), F2)", variables);
+		ArrayList<Initiator> inita2 = new ArrayList<Initiator>();
+		inita2.add((Initiator) Interpreter.stringToDPost("initiates(putdownForks(F1, philosopher(I), F2), available(F1))", variables));
+		inita2.add((Initiator) Interpreter.stringToDPost("initiates(putdownForks(F1, philosopher(I), F2), available(F2))", variables));
+		Action putdown = new Action(a2, inita2, new ArrayList<Terminator>(), null, null);
+		// }
+		DSet D = new DSet(pickup, putdown);
+		// }
+		Database.getInstance().setdSet(D);
+		// }*/
+		/*// ReactiveRule {
+		ReactiveRule rr1 = Interpreter.stringToReactiveRule("timeToEat(philosopher(I)) -> dine(philosopher(I))", null);
+		ReactiveRuleSet.getInstance(rr1);
+		// }*/
+		/*// Goals {
+		// {
+		variables = new HashMap<String, Variable>();
+		RuleSet definition = new RuleSet(Interpreter.stringToRule("dine(philosopher(I)) :- adjacent(F1, philosopher(I), F2) & pickupForks(F1, philosopher(I), F2) & putdownForks(F1, philosopher(I), F2)", variables));
+		Goal dine = new Goal(definition.getRule(0).getHead(), definition);
+		// }
+		GoalSet set = new GoalSet(dine);
+		GoalsList.getInstance(set);
+		// }*/
+		/*// Events {
+		RuleSet events = new RuleSet(Interpreter.stringToRule("timeToEat(philosopher(0))", null),
+				Interpreter.stringToRule("timeToEat(philosopher(1))", null),
+				Interpreter.stringToRule("timeToEat(philosopher(2))", null),
+				Interpreter.stringToRule("timeToEat(philosopher(3))", null),
+				Interpreter.stringToRule("timeToEat(philosopher(4))", null));
+		CycleHandler.getInstance().setEvents(events);
+		//}*/
+	//}
+	
+}
